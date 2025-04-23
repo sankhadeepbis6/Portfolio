093a6fec1b308d6c0899401c890db0a5e9005a7b
Sankhadeep Biswas <sankhadeep.biswas.6@gmail.com>
2025-04-23 23:16:08 +0530

    Footer sample button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from "./Footer.module.css";
 import WaveSVG from '../WaveSVG/WaveSVG';
+import FooterButton from '../FooterButton/FooterButton';
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <WaveSVG />
+            <FooterButton label="Contact Me" onClick={() => console.log("clicked")} />
             <div className={styles.footerContent}>
                 <p className={styles.endText}>&copy; {new Date().getFullYear()} Sankhadeep Biswas. All rights reserved.</p>
             </div>
