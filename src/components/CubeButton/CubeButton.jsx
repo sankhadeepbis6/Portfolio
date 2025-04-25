4cbcabe47435bc7119fa14fad125583f6b2ff4e5
Sankhadeep Biswas <sankhadeep.biswas.6@gmail.com>
2025-04-25 10:52:35 +0530

    Cube button changes

--- src/components/CubeButton/CubeButton.jsx
+++ src/components/CubeButton/CubeButton.jsx
@@ -8,18 +8,20 @@ const CubeButton = ({
     top = "top",
     bottom = "bottom",
     effectOnHover = false,
+    width = 100,
+    height = 100,
 }) => {
     const cubeClass = effectOnHover ? styles.hoverCube : styles.animatedCube;
 
     return (
-        <div className={styles.cubeContainer}>
+        <div className={styles.cubeContainer} style={{ width, height }}>
             <div className={`${styles.cube} ${cubeClass}`}>
-                <div className={`${styles.face} ${styles.front}`}>{front}</div>
-                <div className={`${styles.face} ${styles.back}`}>{back}</div>
-                <div className={`${styles.face} ${styles.right}`}>{right}</div>
-                <div className={`${styles.face} ${styles.left}`}>{left}</div>
-                <div className={`${styles.face} ${styles.top}`}>{top}</div>
-                <div className={`${styles.face} ${styles.bottom}`}>{bottom}</div>
+                <div className={`${styles.face} ${styles.front}`} style={{ transform: `translateZ(${width / 2}px)` }} >{front}</div>
+                <div className={`${styles.face} ${styles.back}`} style={{ transform: `rotateY(180deg) translateZ(${width / 2}px)` }}>{back}</div>
+                <div className={`${styles.face} ${styles.right}`} style={{ transform: `rotateY(90deg) translateZ(${width / 2}px) rotateZ(270deg)` }}>{right}</div>
+                <div className={`${styles.face} ${styles.left}`} style={{ transform: `rotateY(-90deg) translateZ(${width / 2}px)` }}>{left}</div>
+                <div className={`${styles.face} ${styles.top}`} style={{ transform: `rotateX(90deg) translateZ(${width / 2}px) rotateZ(180deg)` }}>{top}</div>
+                <div className={`${styles.face} ${styles.bottom}`} style={{ transform: `rotateX(-90deg) translateZ(${width / 2}px)` }}>{bottom}</div>
             </div>
         </div>
     );
--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -4,7 +4,14 @@ import styles from './Overview.module.css';
 const Overview = () => {
     return (
         <section className={styles.overview}>
-            <CubeButton front={"Overview"} back="Overview" right="Overview" left="Overview" top="Overview" bottom="Overview" effectOnHover={false} />
+            <CubeButton
+                front="Web Apps" back="REST API" right="Agile"
+                left="CI/CD" top="OOPs" bottom="Github"
+                effectOnHover={false} width={150} height={150} />
+            <CubeButton
+                front="Microservices" back="Version Control" right="Cloud"
+                left="Caching" top="Load Balancing" bottom="Unit Test"
+                effectOnHover={false} width={150} height={150} />
         </section>
     );
 };
